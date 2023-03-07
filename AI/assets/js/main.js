77b01d92dabdda92387b1fe12f339723149bd45f
BaakhOfficial <baakhofficial@gmail.com>
2023-03-07 18:56:02 +0300

    update

---
+++ AI/assets/js/main.js
@@ -0,0 +1,20 @@
+const centralNode = document.getElementById('central-node');
+const node1 = document.querySelector('.node:nth-of-type(1)');
+const node2 = document.querySelector('.node:nth-of-type(2)');
+const node3 = document.querySelector('.node:nth-of-type(3)');
+
+centralNode.addEventListener('click', () => {
+  // Perform action when central node is clicked
+});
+
+node1.addEventListener('click', () => {
+  // Perform action when node 1 is clicked
+});
+
+node2.addEventListener('click', () => {
+  // Perform action when node 2 is clicked
+});
+
+node3.addEventListener('click', () => {
+  // Perform action when node 3 is clicked
+});
